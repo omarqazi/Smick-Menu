ef1cfbcb3e1897458c770919db034b4e457b604a
Omar Qazi <omar@smick.co>
2018-09-30 20:19:06 -0700

    Initial Commit

---
+++ Smick Menu/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Smick Menu
+//
+//  Created by Omar Qazi on 9/30/18.
+//  Copyright © 2018 Smick Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
